c2fa49569343645912a37249f6f8136d494e6aec
Riazantsev <riazantsevmark327@gmail.com>
2023-08-04 20:30:49 +0400

    task conditions

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int price = 10_000;
+        int creditАmount = 1_000_000;
+        int monthlyPayment = 1_000_000;
+        int interestRate = 1_000_000;
+        int creditPeriod = 1_000_000;
 
-        
-        int miles = service.calculate(price);
-        System.out.println(miles);
+        int annuityPayment = service.calculate(creditАmount, monthlyPayment, interestRate, creditPeriod);
+        System.out.println(annuityPayment);
     }
 }
